f7c7a02a602806cedb14c99b3b1bb9fc6ef8df13
JimmyLIn <linyuan5856139@163.com>
2021-07-10 18:26:20 +0800

    Server First Commit

---
+++ FunnyServer/FunnyServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunnyServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
